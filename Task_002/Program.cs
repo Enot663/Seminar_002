71db69bb601c499f686c9b7a1425db04e7ce9071
Andrey <andrejsatykov@yandex.ru>
2022-11-29 23:06:30 +0300

    Работа с трехзначными Task 2

---
+++ Task_002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу выводящую случайное трехзначное число
+// и удаляющую его вторую цифру
+
+Console.Clear();
+int randomR = new Random().Next(100, 1000);
+
+Console.WriteLine($"Случайное число: {randomR}");
+
+int number1 = randomR % 10;
+int number2 = randomR / 100;
+
+Console.WriteLine($"Трехзначное число без второй цифры = {number2}{number1}.");
+
+
+
+
+
